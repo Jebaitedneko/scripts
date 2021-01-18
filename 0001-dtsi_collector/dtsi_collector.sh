ba41816e44cbd3b5e21629f9014699d724650385
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2021-01-18 07:00:11 +0530

    dtsi_collector: update
    * also grab the header changes
    * parse out the preprocessing to a list
    * initialize repo automatically after creation

--- 0001-dtsi_collector/dtsi_collector.sh
+++ 0001-dtsi_collector/dtsi_collector.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 fol=${PWD##*/}_${1}
 [ -d ../$fol ] && rm -rf ../$fol && mkdir -p ../$fol || mkdir -p ../$fol
-cat out/arch/arm64/boot/dts/qcom/".$1.dtb.d.pre.tmp" | \
-sort -u | \
-grep -v ".o:" | \
-grep -v "\.h" | \
-grep "\.dts" | \
-sed -e 's/ ..\///g' -e 's/ \\//g' | \
-grep -v "skeleton" | \
-xargs cp -r -t ../$fol
+
+magic=`cat out/arch/arm64/boot/dts/qcom/".$1.dtb.d.pre.tmp" | \
+cut -f 2 -d ':' | \
+sed -e "s/ ..\///g" -e "s/ \///g" | \
+grep -v "skeleton" |
+cut -f 1 -d '\' |
+sort -u`
+
+cmd=$(for l in $magic; do echo $l; done)
+
+cp --parents $cmd ../$fol
+
+( cd ../$fol; git init; git add .; git commit -m "init" )
