5a9db8783b2d6e49e1f7f3131010338d1ba292c7
Jebaitedneko <Jebaitedneko@gmail.com>
2021-04-25 01:15:54 +0530

    arm-magisk-patch: directly patch bootimage

--- 0006-arm-magisk-patch/README.md
+++ 0006-arm-magisk-patch/README.md
@@ -1,18 +1,14 @@
 ## 6. arm-magisk-patch.sh ##
 
 ### About: ###
-kernel patcher for arm devices
-input file should be Image.gz, zImage or Image.gz-dtb (or equivalent)
+boot image magisk patcher for arm devices
 
 ### Usage: ###
-1. `git clone https://github.com/SebaUbuntu/AIK-Linux-mirror AIK && cd AIK`
-2. copy over stock boot image into AIK dir as boot.img
-3. `./unpackimg.sh boot.img`
-4. `cd split_img`
-5. `curl -LSsO https://github.com/Jebaitedneko/scripts/raw/master/0006-arm-magisk-patch/arm-magisk-patch.sh`
-6. `chmod +x arm-magisk-patch.sh`
-7. `./arm-magisk-patch.sh boot.img-zImage`
-8. delete old boot.img-zImage and rename boot.img-zImage_repacked to boot.img-zImage
-9. `cd ..`
-10. `./repackimg.sh`
-11. repacked image will be image-new.img
+
+Assuming boot image name as boot.img
+
+1. `curl -LSsO https://github.com/Jebaitedneko/scripts/raw/master/0006-arm-magisk-patch/arm-magisk-patch.sh`
+2. `chmod +x arm-magisk-patch.sh`
+3. `./arm-magisk-patch.sh boot.img`
+
+Repacked image will be boot_repacked.img
--- 0006-arm-magisk-patch/arm-magisk-patch.sh
+++ 0006-arm-magisk-patch/arm-magisk-patch.sh
@@ -1,55 +1,43 @@
 #!/bin/bash
 
-# carve out arm header
-echo "carving out arm header from ${1} ..."
-dd if=$1 of=arm-header bs=1 skip=0 count=$(grep -obUaPHn "\x1F\x8B\x08" $1 | cut -f3 -d ":" | head -n1)
-echo "done"
+echo -e "\ncarving out boot-headers from ${1}..."
+dd if=$1 of=boot-headers bs=1 skip=0 count=$(grep -obUaPHn "\x00\x00\xA0\xE1\x00\x00" $1 | cut -f3 -d ":" | head -n1)
 
-# carve out Image.gz-dtb
-echo "carving out Image.gz-dtb from ${1} ..."
-dd if=$1 of=Image.gz-dtb bs=1 skip=$(grep -obUaPHn "\x1F\x8B\x08" $1 | cut -f3 -d ":" | head -n1)
-echo "done"
+echo -e "\ncarving out kernel-bits from ${1} ..."
+dd if=$1 of=kernel-bits bs=1 skip=$(grep -obUaPHn "\x00\x00\xA0\xE1\x00\x00" $1 | cut -f3 -d ":" | head -n1)
 
-# carve out dtbs from Image.gz-dtb
-echo "carving out dtbs from Image.gz-dtb ..."
-dd if=Image.gz-dtb of=dtbs bs=1 skip=$(echo $(grep -obUaPHn "\xD0\x0D\xFE\xED" $1) | cut -f3 -d ":" | head -n1)
-echo "done"
+echo -e "\ncarving out arm header from kernel-bits ..."
+dd if=kernel-bits of=arm-header bs=1 skip=0 count=$(grep -obUaPHn "\x1F\x8B\x08" kernel-bits | cut -f3 -d ":" | head -n1)
 
-# unpack Image.gz-dtb to Image.gz (dtbs will be ignored, need to be appended later)
-echo "gunzip Image.gz-dtb in progress ..."
-mv Image.gz-dtb Image.gz # for allowing gunzip to view it
+echo -e "\ncarving out Image.gz-dtb from kernel-bits ..."
+dd if=kernel-bits of=Image.gz-dtb bs=1 skip=$(grep -obUaPHn "\x1F\x8B\x08" kernel-bits | cut -f3 -d ":" | head -n1)
+
+rm kernel-bits
+
+echo -e "\ncarving out dtbs from Image.gz-dtb ..."
+dd if=Image.gz-dtb of=dtbs bs=1 skip=$(grep -obUaPHn "\xD0\x0D\xFE\xED" Image.gz-dtb | cut -f3 -d ":" | head -n1)
+
+echo -e "\ngunzip Image.gz-dtb in progress ..."
+mv Image.gz-dtb Image.gz
 gunzip -N Image.gz
-echo "done"
 
-# patch for magisk
-# skip_initramfs -> want_initramfs
-echo "patching for magisk (skip_initramfs -> want_initramfs) ..."
+echo -e "\npatching for magisk (skip_initramfs -> want_initramfs) ..."
 sed -i "s|\x73\x6b\x69\x70\x5f\x69\x6e\x69\x74\x72\x61\x6d\x66\x73|\x77\x61\x6e\x74\x5f\x69\x6e\x69\x74\x72\x61\x6d\x66\x73|g" Image
-echo "done"
 
-# repack Image to Image.gz
-echo "gzip Image in progress ..."
-gzip -N Image
-echo "done"
+echo -e "\ngzip Image in progress ..."
+gzip -N9 Image
 
-# apend dtbs to the end of Image.gz
-echo "Appending dtbs to the end of Image.gz ..."
-cat dtbs >> Image.gz
-echo "done"
+echo -e "\nAppending dtbs to the end of Image.gz ..."
+cat dtbs >> Image.gz && rm dtbs
 
-# now it's Image.gz-dtb
-echo "Renaming Image.gz to Image.gz-dtb ..."
+echo -e "\nRenaming Image.gz to Image.gz-dtb ..."
 mv Image.gz Image.gz-dtb
-echo "done"
 
-# apend Image.gz-dtb to the end of arm header
-echo "Appending Image.gz-dtb to the end of arm header ..."
-cat Image.gz-dtb >> arm-header
-echo "done"
+echo -e "\nAppending Image.gz-dtb to the end of arm header ..."
+cat Image.gz-dtb >> arm-header && rm Image.gz-dtb
 
-# now it's repacked
-echo "Renaming to ${1}_repacked"
-mv arm-header ${1}_repacked
-rm Image.gz-dtb # remove stray file
-rm dtbs
-echo "done"
+echo -e "\nAppending arm-header to the end of boot-headers ..."
+cat arm-header >> boot-headers && rm arm-header
+
+echo -e "\nRenaming to ${1::-4}_repacked.img"
+mv boot-headers ${1::-4}_repacked.img
