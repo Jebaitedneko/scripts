fbcd359a249f0f77ad6f8f8ea1ce8324716db9d7
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-11 09:32:00 +0530

    arm-magisk-patch: update

--- 0006-arm-magisk-patch/arm-magisk-patch.sh
+++ 0006-arm-magisk-patch/arm-magisk-patch.sh
@@ -10,13 +10,13 @@ echo -e "\ncarving out dtbs from ${1} ..."
 dd if=$1 of=dtbs bs=1 skip=$(grep -obaP "\xD0\x0D\xFE\xED" Image.gz-dtb | cut -f1 -d ":" | head -n1)
 
 echo -e "\ngunzip Image.gz-dtb in progress ..."
-mv Image.gz-dtb Image.gz && gunzip -N Image.gz
+mv Image.gz-dtb Image.gz && 7z x Image.gz
 
 echo -e "\npatching for magisk (skip_initramfs -> want_initramfs) ..."
 sed -i "s|\x73\x6b\x69\x70\x5f\x69\x6e\x69\x74\x72\x61\x6d\x66\x73|\x77\x61\x6e\x74\x5f\x69\x6e\x69\x74\x72\x61\x6d\x66\x73|g" Image
 
 echo -e "\ngzip Image in progress ..."
-gzip -N9 Image
+7z a -mx9 Image.gz Image
 
 echo -e "\nAppending dtbs to the end of Image.gz ..."
 cat dtbs >> Image.gz && rm dtbs && mv Image.gz Image.gz-dtb
