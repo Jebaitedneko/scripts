127033cea3c45f58ab6e52a8a34cca1fc4c2f96d
Jebaitedneko <Jebaitedneko@gmail.com>
2021-05-11 09:31:45 +0530

    dtsi_collector: update

--- 0001-dtsi_collector/dtsi_collector.sh
+++ 0001-dtsi_collector/dtsi_collector.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
-fol=${PWD##*/}_${1}
-[ -d ../$fol ] && rm -rf ../$fol && mkdir -p ../$fol || mkdir -p ../$fol
 
-magic=`cat out/arch/arm64/boot/dts/qcom/".$1.dtb.d.pre.tmp" | \
-cut -f 2 -d ':' | \
-sed -e "s/ ..\///g" -e "s/ \///g" | \
-grep -v "skeleton" |
-cut -f 1 -d '\' |
-sort -u`
+folder=${PWD##*/}_${1}
+[ -d ../$folder ] && rm -rf ../$folder && mkdir -p ../$folder || mkdir -p ../$folder
 
-cmd=$(for l in $magic; do echo $l; done)
+if [[ $(find out/arch/arm64/boot/dts/qcom -type f -iname ".$1.dtbo.d.pre.tmp") ]]; then
+	echo -e "\nDTBO Target Detected."
+	file=".$1.dtbo.d.pre.tmp"
+else
+	if [[ $(find out/arch/arm64/boot/dts/qcom -type f -iname ".$1.dtb.d.pre.tmp") ]]; then
+		echo -e "\nDTB Target Detected."
+		file=".$1.dtb.d.pre.tmp"
+	fi
+fi
 
-cp --parents $cmd ../$fol
+echo "
+$(
+	cat out/arch/arm64/boot/dts/qcom/"$file" | \
+	cut -f 2 -d ':' | \
+	sed -e "s/ ..\///g" -e "s/ \///g" | \
+	grep -v "skeleton" |
+	cut -f 1 -d '\' |
+	sort -u
+)
+" | while read file; do cp --parents $file ../$folder; done
 
-( cd ../$fol; git init; git add .; git commit -m "init" )
+( cd ../$folder; git init; git add .; git commit -m "init" )
