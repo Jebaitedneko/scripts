8b89ed70975506bfcb2c303c754d14ffb3359b3c
Jebaitedneko <Jebaitedneko@gmail.com>
2021-11-13 03:28:08 +0530

    add touch fw dumper
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

---
+++ 0007-touch-fw-dumper/README.md
@@ -0,0 +1,10 @@
+## 7. dump_touch_fw.sh ##
+
+### About: ###
+dump touch fw for poco x3 pro from an input direct fastboot rom link
+
+### Usage: ###
+
+1. `curl -LSsO https://github.com/Jebaitedneko/scripts/raw/master/0007-touch-fw-dumper/dump_touch_fw.sh`
+2. `chmod +x dump_touch_fw.sh`
+3. `./dump_touch_fw.sh direct_rom_link_here`
---
+++ 0007-touch-fw-dumper/dump_touch_fw.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# apt install aria2 gcc-avr xxd &> /dev/null
+rom_link="$1"
+rom_name=$(echo $rom_link | sed 's/.*\///;s/.zip//g')
+echo $rom_name > info.txt
+[ ! -f "${rom_name}.zip" ] && aria2c -j32 "$rom_link"
+unzip -p *.zip boot.img > boot.img
+
+[ ! -f unpack_bootimg.py ] && wget "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz" && tar -xf master.tar.gz unpack_bootimg.py && rm master.tar.gz
+chmod +x unpack_bootimg.py
+python3 unpack_bootimg.py --boot_img boot.img --out . &> /dev/null
+
+xxd -g 1 kernel | grep "40 71 02 00" -B1 -A8702 | grep -oE "[0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+" > firmware.xxd
+xxd -r -p <(sed -n '1,8704'p firmware.xxd      | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_fw01.bin
+xxd -r -p <(sed -n '8705,17408'p firmware.xxd  | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_mp01.bin
+xxd -r -p <(sed -n '17409,26112'p firmware.xxd | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_fw02.bin
+xxd -r -p <(sed -n '26113,34816'p firmware.xxd | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_mp02.bin
+
+find -type f -iname "*.bin" -exec avr-objcopy -I binary -O ihex {} {}.ihex \;
+
+md5sum *.bin *.ihex >> info.txt
+
+ZIPNAME="$(cat info.txt | head -n1)"_FW.zip
+zip -r9 $ZIPNAME *.bin *.ihex info.txt
+rm *.bin *.ihex info.txt dtb ramdisk boot.img firmware.xxd kernel
+echo "$(curl -s -F f[]=@$ZIPNAME "https://oshi.at" | grep DL | sed 's/DL: //g')"
