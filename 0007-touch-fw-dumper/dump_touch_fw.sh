2863167b2a60f04a7d02dd3d63956407cf024dcf
Jebaitedneko <Jebaitedneko@gmail.com>
2021-11-13 12:16:14 +0530

    touch_fw_dumper: isolate each job for parallelization
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- 0007-touch-fw-dumper/dump_touch_fw.sh
+++ 0007-touch-fw-dumper/dump_touch_fw.sh
@@ -3,6 +3,19 @@
 ROM_LINK="$1"
 
 ROM_NAME=$(echo "$ROM_LINK" | sed 's/.*\///;s/.zip//g')
+ROM_NAME_MIN="$(echo "$ROM_NAME" | sed 's/miui_VAYU//g;s/Global//g;s/^_/GL_/g')"
+
+if [ ! -d "$ROM_NAME_MIN" ]; then
+	mkdir -p "$ROM_NAME_MIN"
+fi
+
+if [ ! -f unpack_bootimg.py ]; then
+	wget "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz" &> /dev/null
+	tar -xf master.tar.gz unpack_bootimg.py && chmod +x unpack_bootimg.py && rm master.tar.gz
+fi
+
+cd "$ROM_NAME_MIN"
+
 echo "$ROM_NAME" > info.txt
 
 if [ ! -f "${ROM_NAME}.zip" ]; then
@@ -11,12 +24,7 @@ fi
 
 unzip -p "${ROM_NAME}.zip" boot.img > boot
 
-if [ ! -f unpack_bootimg.py ]; then
-	wget "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz" &> /dev/null
-	tar -xf master.tar.gz unpack_bootimg.py && chmod +x unpack_bootimg.py && rm master.tar.gz
-fi
-
-python3 unpack_bootimg.py --boot_img boot --out . &> /dev/null
+python3 "../unpack_bootimg.py" --boot_img boot --out . &> /dev/null
 
 xxd -g 1 kernel | grep "40 71 02 00" -B1 -A8702 | grep -oE "[0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+" > firmware.xxd
 
@@ -33,12 +41,18 @@ sed -i "s/\r//g" ./*.ihex
 
 md5sum ./*.bin ./*.ihex >> info.txt
 
-ZIPNAME="$(echo "$ROM_NAME" | sed 's/miui_VAYU//g;s/Global//g')_TOUCH_FW.zip"
+ZIPNAME="${ROM_NAME_MIN}_TOUCH_FW.zip"
 zip -r9 "$ZIPNAME" ./*.bin ./*.ihex info.txt firmware.xxd kernel &> /dev/null
 
 rm ./*.bin ./*.ihex info.txt dtb ramdisk boot firmware.xxd kernel "${ROM_NAME}.zip"
 
 if [[ $2 != '' && $2 == 'u' ]]; then
 	UPLOAD=$(curl -s -F f[]=@"$ZIPNAME" "https://oshi.at" | grep DL | sed 's/DL: //g')
-	echo -e "$UPLOAD\n" >> .links
+	echo -e "$UPLOAD\n" >> ../links.txt
 fi
+
+mv "$ZIPNAME" ../
+
+cd ../
+
+rm -rf "$ROM_NAME_MIN"
