02e45211d65939afd583b2e18191b0aa780e3258
Jebaitedneko <Jebaitedneko@gmail.com>
2021-11-13 11:44:23 +0530

    touch_fw_dumper: prettify
    Signed-off-by: Jebaitedneko <Jebaitedneko@gmail.com>

--- 0007-touch-fw-dumper/dump_touch_fw.sh
+++ 0007-touch-fw-dumper/dump_touch_fw.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 rom_link="$1"
+
 rom_name=$(echo "$rom_link" | sed 's/.*\///;s/.zip//g')
 echo "$rom_name" > info.txt
-[ ! -f "${rom_name}.zip" ] && aria2c -j32 "$rom_link" &> /dev/null
+
+if [ ! -f "${rom_name}.zip" ]; then
+	aria2c -j32 "$rom_link" &> /dev/null
+fi
+
 unzip -p "${rom_name}.zip" boot.img > boot
 
-[ ! -f unpack_bootimg.py ] && wget "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz" &> /dev/null && tar -xf master.tar.gz unpack_bootimg.py && rm master.tar.gz
-chmod +x unpack_bootimg.py
+if [ ! -f unpack_bootimg.py ]; then
+	wget "https://android.googlesource.com/platform/system/tools/mkbootimg/+archive/refs/heads/master.tar.gz" &> /dev/null
+	tar -xf master.tar.gz unpack_bootimg.py && chmod +x unpack_bootimg.py && rm master.tar.gz
+fi
+
 python3 unpack_bootimg.py --boot_img boot --out . &> /dev/null
 
 xxd -g 1 kernel | grep "40 71 02 00" -B1 -A8702 | grep -oE "[0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+ [0-9a-f]+" > firmware.xxd
-xxd -r -p <(sed -n '1,8704p' firmware.xxd      | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_fw01.bin
-xxd -r -p <(sed -n '8705,17408p' firmware.xxd  | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_mp01.bin
+
+xxd -r -p <(sed -n '1,8704p'      firmware.xxd | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_fw01.bin
+xxd -r -p <(sed -n '8705,17408p'  firmware.xxd | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_mp01.bin
 xxd -r -p <(sed -n '17409,26112p' firmware.xxd | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_fw02.bin
 xxd -r -p <(sed -n '26113,34816p' firmware.xxd | sed "s/ //g" | tr -d '\n') > j20s_novatek_ts_mp02.bin
 
-find . -type f -iname "*.bin" -exec avr-objcopy -I binary -O ihex {} {}.ihex \;
 sed -i "s/\r//g" ./*.bin
+
+find . -type f -iname "*.bin" -exec avr-objcopy -I binary -O ihex {} {}.ihex \;
+
 sed -i "s/\r//g" ./*.ihex
 
 md5sum ./*.bin ./*.ihex >> info.txt
